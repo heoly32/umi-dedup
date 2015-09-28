cb4a417a6f70556d0b7a133f66275eed26c9d6ab
jwfoley <joseph.foley@mail.mcgill.ca>
2015-09-28 13:43:31 -0400

    refactored deduplication for count vector input and implemented quality sorting, added option to provide a table of prior frequencies, various cleanup

--- lib/read_buffer.py
+++
@@ -1,9 +0,0 @@
-import collections
-from . import parse_sam
-
-class ReadBuffer(collections.deque()):
-	'''
-	Queue of SAM reads grouped by start (5') position. Expects to have reads added in order of leftmost position (standard SAM sorting) and produces them in the same order. This allows collecting reads by start position without breaking the sort order.
-	'''
-	def popleft():
-		
