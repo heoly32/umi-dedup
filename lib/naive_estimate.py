407580f73eecb36eff3d58aeca805f286b4b7453
jwfoley <joseph.foley@mail.mcgill.ca>
2015-09-28 13:46:10 -0400

    refactored deduplication for count vector input and implemented quality sorting, added option to provide a table of prior frequencies, various cleanup

--- lib/naive_estimate.py
+++ lib/naive_estimate.py
@@ -5,3 +5,8 @@ def estimate_p (umi_counts):
 	n_umi = sum(count > 0 for count in umi_counts.values())
 	return n_umi / n_hit
 
+def deduplicate_counts (umi_counts):
+	for key in umi_counts:
+		if umi_counts[key] > 0: umi_counts[key] = 1
+	return umi_counts
+
