01e790ae8b67acfb26bc512f2b4871cb1ae42b66
MTurgeon <maxime.turgeon@mail.mcgill.ca>
2015-10-09 17:09:01 -0400

    algorithm now seems to work

--- dedup.py
+++ dedup.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
 import collections, argparse, pysam, sys
-from lib import parse_sam, umi_data, optical_duplicates, naive_estimate
+from lib import parse_sam, umi_data, optical_duplicates, naive_estimate, uniform_estimate
 
 # parse arguments
 parser = argparse.ArgumentParser(description = 'Read a coordinate-sorted BAM file with labeled UMIs and mark or remove duplicates due to PCR or optical cloning, but not duplicates present in the original library. When PCR/optical duplicates are detected, the reads with the highest total base qualities are marked as non-duplicate - note we do not discriminate on MAPQ, or other alignment features, because this would bias against polymorphisms.')
@@ -11,6 +11,7 @@ parser_alg = parser.add_argument_group('algorithm')
 parser_perf = parser.add_argument_group('performance testing')
 parser_format.add_argument('-r', '--remove', action = 'store_true', help = 'remove PCR/optical duplicates instead of marking them')
 parser_alg.add_argument('-d', '--dist', action = 'store', type = int, default = 100, help = 'maximum pixel distance for optical duplicates (Euclidean); set to 0 to skip optical duplicate detection')
+parser_alg.add_argument('-a', '--algorithm', action = 'store', default = 'naive', help = 'algorithm for duplicate identification')
 parser_perf.add_argument('--truncate_umi', action = 'store', type = int, default = None, help = 'truncate UMI sequences to this length')
 parser_data.add_argument('in_file', action = 'store', nargs = '?', default = '-', help = 'input BAM')
 parser_data.add_argument('out_file', action = 'store', nargs = '?', default = '-', help = 'output BAM')
@@ -37,11 +38,11 @@ def pop_buffer(): # pop the oldest read off the buffer (into the output), but fi
 	read = read_buffer.popleft()
 	start_pos, umi = parse_sam.get_start_pos(read), umi_data.get_umi(read.query_name, args.truncate_umi)
 	this_pos = pos_tracker[read.is_reverse][start_pos]
-	
+
 	# deduplicate reads at this position
 	if not this_pos['deduplicated']:
 		umi_reads = this_pos['reads']
-		
+
 		# first pass: mark optical duplicates
 		if args.dist != 0:
 			all_reads = [] # combine reads from all UMIs so optical duplicates aren't required to have matching UMIs (maybe there was a sequencing error)
@@ -50,30 +51,30 @@ def pop_buffer(): # pop the oldest read off the buffer (into the output), but fi
 				for dup_read in umi_data.mark_duplicates(opt_dup, len(opt_dup) - 1):
 					if dup_read.is_duplicate: umi_reads[umi_data.get_umi(dup_read.query_name, args.truncate_umi)].remove(dup_read) # remove duplicate reads from the tracker so they won't be considered later (they're still in the read buffer)
 				read_counter['optical duplicate'] += len(opt_dup) - 1
-		
+
 		# second pass: mark PCR duplicates
 		umi_counts = umi_data.make_umi_counts(umi_totals.keys())
 		for umi, reads in umi_reads.items(): umi_counts[umi] = len(reads)
-		
+
 		# P ESTIMATION / DEDUPLICATION GOES HERE
-		# if args.algorithm == 'naive':
-	        dedup_counts = naive_estimate.deduplicate_counts(umi_counts)
-		
-		# if args.algorithm == 'uniform':
-		#     dedup_counts = uniform_estimate.deduplicate_counts(umi_counts)
+		if args.algorithm == 'naive':
+			dedup_counts = naive_estimate.deduplicate_counts(umi_counts)
+
+		if args.algorithm == 'uniform':
+		    dedup_counts = uniform_estimate.deduplicate_counts(umi_counts)
 
 		for umi, reads in umi_reads.items():
 			n_dup = len(reads) - dedup_counts[umi]
 			umi_data.mark_duplicates(reads, n_dup)
 			read_counter['PCR duplicate'] += n_dup
-		
+
 		read_counter['unique'] += sum(dedup_counts.values())
 
 		this_pos['deduplicated'] = True
-	
+
 	# output read
 	if not (args.remove and read.is_duplicate): out_bam.write(read)
-	
+
 	# prune the tracker
 	if read is this_pos['last read']: del pos_tracker[read.is_reverse][start_pos]
 
@@ -96,11 +97,11 @@ for read in in_bam:
 	read.is_duplicate = False # not sure how to handle reads that have already been deduplicated somehow, so just ignore previous annotations
 	start_pos = parse_sam.get_start_pos(read)
 	read_counter['read'] += 1
-	
+
 	# advance the buffer
 	while read_buffer and (read_buffer[0].reference_id < read.reference_id or parse_sam.get_start_pos(read_buffer[0]) < read.reference_start): pop_buffer() # pop the top read if it's at a position that's definitely not going to get any more hits
-	
-	# add read to buffer and tracking data structure	
+
+	# add read to buffer and tracking data structure
 	read_buffer.extend([read])
 	try:
 		pos_tracker[read.is_reverse][start_pos]['reads'][umi] += [read]
