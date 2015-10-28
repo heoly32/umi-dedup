082b109a2371e17f6ccf3a773bf1ac6b32987f9f
MTurgeon <maxime.turgeon@mail.mcgill.ca>
2015-10-28 11:23:00 -0400

    implemented total prop estimation

--- lib/bayes_estimate.py
+++ lib/bayes_estimate.py
@@ -1,7 +1,7 @@
 from __future__ import division
 import numpy as np
 import collections
-import MCMC_algorithm
+import MCMC_algorithm_pi
 import sys
 
 DEFAULT_NSAMP = 1000
@@ -34,21 +34,26 @@ def deduplicate_counts (umi_counts, nsamp=DEFAULT_NSAMP, nthin=DEFAULT_NTHIN, nb
         C_prior = [1./n] * n
     else:
         # The non-uniform algorithm illicits prior from data
-        N_total = sum(total_counts.values())
-        C_prior = [float(total_data[j])/N_total for j in range(n)]
-        C_prior = [C_prior[j]/min(C_prior) for j in range(n)]
+        N_total = sum(total_data)
+        C_prior = [float(1.5 * total_data[j])/N_total for j in range(n)]
+        # Uncomment next line to use only current data to illicit prior
+        # C_prior = [float(data[j])/N for j in range(n)]
 
     # Run Gibbs sampler
-    p_post = MCMC_algorithm.MCMC_algorithm(data, \
-                                           n, N, \
-                                           S_prior, C_prior, pi_prior, \
-                                           nsamp, nthin, nburn, \
-                                           True)
+    pi_post = MCMC_algorithm_pi.MCMC_algorithm(data, \
+                                            n, N, \
+                                            S_prior, C_prior, pi_prior, \
+                                            nsamp, nthin, nburn, \
+                                            True)
 
-    # Compute median for each tag
-    median_list = [0] * n
-    for i in range(n):
-        median_list[i] = computeMedian(p_post[i::n])
+    # # Compute median for each tag
+    # median_list = [0] * n
+    # for i in range(n):
+    #     median_list[i] = computeMedian(p_post[i::n])
+
+    # Distribute counts across tags
+    p = computeMedian(pi_post)
+    data_dedup = apportion_counts(data, round(p * sum(data)))
 
     # Return ordered dictionary with estimated number of true molecules
     umi_true = collections.OrderedDict()
@@ -57,7 +62,8 @@ def deduplicate_counts (umi_counts, nsamp=DEFAULT_NSAMP, nthin=DEFAULT_NTHIN, nb
         if value == 0:
             umi_true[key] = value
         else:
-            umi_true[key] = int(np.ceil(median_list[index] * data[index]))
+            # umi_true[key] = int(np.ceil(median_list[index] * data[index]))
+            umi_true[key] = int(data_dedup[index])
             index += 1
 
     return umi_true
@@ -66,10 +72,23 @@ def computeMedian(list):
     list.sort()
     lens = len(list)
     if lens % 2 != 0:
-        midl = (lens / 2)
+        midl = int(lens / 2)
         res = list[midl]
     else:
         odd = int((lens / 2) -1)
         ev = int((lens / 2))
         res = float(list[odd] + list[ev]) / float(2)
     return res
+
+def apportion_counts (counts, target_sum):
+    divisor = float(target_sum) / sum(counts)
+    quotients = (count / divisor for count in counts)
+    result = [int(count > 0) for count in counts]
+    residuals = [quotient - new_count for quotient, new_count in zip(quotients, result)]
+    remaining_counts = target_sum - sum(result)
+    while remaining_counts > 0:
+        which_to_increment = residuals.index(max(residuals))
+        result[which_to_increment] += 1
+        residuals[which_to_increment] -= 1
+        remaining_counts -= 1
+    return result
