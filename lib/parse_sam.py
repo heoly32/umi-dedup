342d24f7f00bf0db5420ce4e6429a780b9c7df64
Joe Foley <jwfoley+git@stanford.edu>
2016-12-02 10:37:50 -0800

    ParsedAlignment class

--- lib/parse_sam.py
+++ lib/parse_sam.py
@@ -1,5 +1,8 @@
 import collections, warnings, copy, pysam
 
+# much of this assumes Illumina-formatted read names
+
+
 def alignment_is_good (alignment):
 	return not (alignment.is_unmapped or alignment.is_secondary or alignment.is_supplementary) # only count primary alignments, and discard unmapped reads since it's too expensive (and useless?) to find their duplicates
 
@@ -38,4 +41,28 @@ Coords = collections.namedtuple('Coords', ['tile', 'x', 'y']) # "tile" actually 
 def get_coords (alignment):
 	tile, x_string, y_string = alignment.query_name.rsplit(':', 3)[:3]
 	return Coords(tile, int(x_string), int(y_string))
+
+class ParsedAlignment:
+	'''
+	container for useful information parsed out of a pysam.AlignedSegment
+	'''
+	def __init__ (self, alignment):
+		self.is_good = 							alignment_is_good(alignment)
+		self.is_properly_paired = 	alignment_is_properly_paired(alignment)
+		self.start_pos = 						get_start_pos(alignment)
+		self.mate_start_pos = 			get_mate_start_pos(alignment)
+		self.is_first_mate = 				is_first_mate(alignment)
+		self.quality_sum = 					get_quality_sum(alignment)
+		self.cluster_coords = 			get_cluster_coords(alignment)
+		self.umi = 									get_umi(alignment.query_name)
+		self.is_reverse = 					alignment.is_reverse
+		self.is_paired = 						alignment.is_paired
+		self.is_duplicate = 				alignment.is_duplicate
+		self.left_pos = 						alignment.reference_start
+		self.reference_id = 				alignment.reference_id
+		self.name = 								alignment.query_name
+	
+	def unparse (self):
+		self.alignment.is_duplicate = self.is_duplicate
+		return self.alignment
 
