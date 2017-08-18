fix underflow issue

lib/poisson_mixture.py
@@ -1,28 +1,34 @@
 import numpy as np
 import math
+import collections
+from . import apportion_counts, umi_data
 
 K_MAX = 10
 
 # Store results into object
 class BICResults:
-  def __init__(self, bic, estimate):
+  def __init__(self, bic, estimate, k):
     self.bic = bic
     self.estimate = estimate
+    self.size = k
 
 # Functions to compute likelihood
 def dpois(x, mu):
-  return np.exp(-mu) * mu**x / math.gamma(x+1)
+  return x*np.log(mu) - mu - math.lgamma(x+1)
 
 def mixture_dist(obs, param):
   n = obs.size
   k = param[0].size
   tmp = np.zeros( (n, k) )
   for j in range(n):
-    tmp[j,...] = param[0] * dpois(obs[j], param[1])
-  return np.sum(tmp, axis = 1)
+    tmp[j,...] = np.log(param[0])  + dpois(obs[j], param[1])
+  if k == 1:
+    return tmp
+  else:
+    return np.logaddexp.reduce(tmp, 1)
 
 def likelihood(data, obs, param):
-  return np.sum(data * np.log(mixture_dist(obs, param)))
+  return np.sum(data * mixture_dist(obs, param))
 
 # BIC for selecting number of components
 def BIC(data, obs, param):
@@ -35,24 +41,25 @@ def mixing_weights(obs, param):
     n = obs.size
     output = np.zeros((n, k))
     for j in range(n):
-      output[j,...] = param[0] * dpois(obs[j], param[1])/np.sum(param[0] * dpois(obs[j], param[1]))
+      output[j,...] = (np.log(param[0]) + dpois(obs[j], param[1])) - np.logaddexp.reduce(np.log(param[0]) + dpois(obs[j], param[1]))
     return output
 
-def gradient(data, obs, param):
-  k = param[0].size
-  mixing_mat = mixing_weights(obs, param)
-  for j in range(k):
-    mixing_mat[...,j] *= data
-  gradient_prob = param[0] * np.sum(mixing_mat, axis = 0)
-  gradient_theta = - np.sum(mixing_mat, axis = 0)
-  for j in range(k):
-    mixing_mat[...,j] *= obs
-  gradient_theta += np.sum(mixing_mat, axis = 0)/param[1]
-  return np.concatenate((gradient_prob, gradient_theta))
+# def gradient(data, obs, param):
+#   k = param[0].size
+#   mixing_mat = mixing_weights(obs, param)
+#   for j in range(k):
+#     mixing_mat[...,j] += np.log(data)
+#   gradient_prob = param[0] * np.logaddexp.reduce(mixing_mat, axis = 0)
+#   gradient_theta = - np.logaddexp.reduce(mixing_mat, axis = 0)
+#   mixing_mat = np.exp(mixing_mat)
+#   for j in range(k):
+#     mixing_mat[...,j] *= obs
+#   gradient_theta += np.sum(mixing_mat, axis = 0)/param[1]
+#   return np.concatenate((gradient_prob, gradient_theta))
 
 def update_param(data, obs, param):
   k = param[0].size
-  mixing_mat = mixing_weights(obs, param)
+  mixing_mat = np.exp(mixing_weights(obs, param))
   for j in range(k):
     mixing_mat[...,j] *= data
   next_prob =np.sum(mixing_mat, axis = 0)/np.sum(data)
@@ -119,11 +126,32 @@ def QN1_algorithm(data, obs, init_param):
               break
     # Compute BIC
     bic = BIC(data, obs, next_param)
-    output = BICResults(bic, next_param)
+    output = BICResults(bic, next_param, K)
     return output
 
 def select_num_comp(data, obs):
   n = data.size
-  bic_list = [QN1_algorithm(data, obs, (np.array(k*[1.0/k]), np.arange(1, k+1))) for k in range(1, min(K_MAX, n))]
+  bic_list = [QN1_algorithm(data, obs, (np.array(k*[1.0/k]), np.arange(1, k+1))) for k in range(1, min(K_MAX, n) + 1)]
   min_bic_result = min(bic_list, key = lambda p: p.bic)
   return min_bic_result
+
+def dedup_cluster(umi_counts):
+  naive_est = len(umi_counts.nonzero_keys())
+  counter = collections.Counter(umi_counts.values())
+  data = obs = []
+  for count_item, data_item in counter.iteritems(): # PYTHON 2 ALERT
+    obs.append(count_item)
+    data.append(data_item)
+  data = np.array(data)
+  obs = np.array(obs)
+  if data.size <= 2:
+    est = naive_est
+  else:
+    min_bic_result = select_num_comp(data, obs)
+    est = 0
+    num_mol = np.arange(min_bic_result.size)
+    mixing_mat = np.exp(mixing_weights(obs, min_bic_result.estimate))
+    for i in range(data.size):
+      est += np.dot(mixing_mat[i,...], num_mol) * obs[i]
+    data_dedup = apportion_counts.apportion_umi_values(umi_counts, max(int(round(est)), naive_est))
+    return umi_data.UmiValues(zip(umi_list, data_dedup))