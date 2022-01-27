c0f5a5253fadf40337d6ae492c968492c527b3b0
LyongH <37587081+heoly32@users.noreply.github.com>
2022-01-26 22:53:18 -0800

    Update optical_duplicates.py
    Change way to calculate distances using pdist(x, squeclidean) in order to improve the performance when comparing distances between reads that mostly have values higher than given MAX DISTANCE

--- lib/optical_duplicates.py
+++ lib/optical_duplicates.py
@@ -1,10 +1,9 @@
 import collections
 from . import parse_sam
+import numpy as np
+from scipy.spatial.distance import pdist, squareform
 
 DEFAULT_DIST = 100
-
-def are_optical_duplicates (coords1, coords2, max_dist):
-	return (coords1.x - coords2.x) ** 2 + (coords1.y - coords2.y) ** 2 <= max_dist
 
 def get_optical_duplicates (reads, max_dist): # return a list where each element is a list of the reads that are optical duplicates of each other (each read is within max_dist of at least one other read in the set)
 	coords_by_tile = {} # group reads by which tile they came from; each value is another dictionary, in which the key is the coordinates and the value is the read itself
@@ -18,11 +17,25 @@ def get_optical_duplicates (reads, max_dist): # return a list where each element
 	result = [] # each element will be a list of reads that are optical duplicates of each other (within max_dist pixels)
 	for tile_reads in coords_by_tile.values():
 		if len(tile_reads) > 1:
+			# Calculate all distance of points in tile reads
+			# Create coordinates matrix in numpy array
+			coord_mat = np.array([[tile_reads[i][1].x, tile_reads[i][1].y] for i in range(len(tile_reads))])
+			# Calculate all distance of points in matrix using the squared Euclidean distance
+			dist = squareform(pdist(coord_mat, 'sqeuclidean')) # (x1 - x2) ** 2 + (y1 - y2) ** 2
+			# Set values at (0, 0), (1,1), (2,2) .. which have zero to max_dist + 1
+			for i in range(len(tile_reads)):
+				dist[i][i] = max_dist + 1
+			# Compare if there are values which equal to or smaller than max_dist
+			dist_logi = dist <= max_dist
+
 			which_group = list(range(len(tile_reads))) # directory of which group each read is in; initially each read is in its own group
 			groups = [[read[0]] for read in tile_reads] # list of all the groups
 			for i in range(len(tile_reads) - 1):
+				if not dist_logi[i].any(): # If there is no True, skip i
+					continue
+
 				for j in range(i + 1, len(tile_reads)):
-					if are_optical_duplicates(tile_reads[i][1], tile_reads[j][1], max_dist):
+					if dist_logi[i][j]: # See if distance between ith and jth is equal to or smaller than max_dist
 						groups[which_group[i]] += groups[which_group[j]] # move the second read's entire group into the first read's group
 						groups[which_group[j]] = [] # delete the second read's group so it won't be duplicated
 						which_group[j] = which_group[i] # point the second read to the first read's group
